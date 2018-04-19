ea451d02fd40e83fc8c83e991b904c6ea6c6a73e
ypurwantopdx <yenny.purwanto@oit.edu>
2018-04-19 14:53:05 -0700

    Customized Set OnClick for submitButton

--- app/src/main/java/com/example/yennywright/happybirthdaytoyou/MainActivity.java
+++ app/src/main/java/com/example/yennywright/happybirthdaytoyou/MainActivity.java
@@ -8,13 +8,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     List<PersonDatabase> personDatabaseList;
-    Validator validator;
+    List<String> allMonthFullWord = Arrays.asList("january", "february", "march", "april", "may",
+                    "june", "july", "august", "september", "october", "november", "december");
+    List<String> allMonthAbr = Arrays.asList("jan", "feb", "mar", "apr", "may",
+                                            "jun", "jul", "aug", "sept", "oct", "nov", "dec");
+    List<String> allMonthInNumber = Arrays.asList("1", "2", "3", "4", "5",
+                                                        "6", "7", "8", "9", "10", "11", "12");
+    List<String> shortMonth = Arrays.asList("february");
+    List<String> mediumMonth = Arrays.asList("april", "june", "september", "november");
+    List<String> longMonth = Arrays.asList("january", "march", "may", "july", "august",
+            "october", "december");
+    TextView outFinalMessageBox;
+    TextView countDisplayBox;
     int EntryCount = 0;
+    EditText nameText;
+    EditText monthText;
+    EditText dayText;
+    Button submitButton;
+    Button resetButton;
+    String finalName;
+    String finalMonth;
+    Integer finalDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,28 +43,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //initializing views
-        EditText nameText = findViewById(R.id.edittext_name_input);
-        nameText.addTextChangedListener(validator.nameTextWatcher);
-        String nameTextStr = nameText.getText().toString();
+        outFinalMessageBox = findViewById(R.id.textview_output_mssg);
+        countDisplayBox = findViewById(R.id.textview_count_display);
 
-        EditText monthText = findViewById(R.id.edittext_month_input);
-        monthText.addTextChangedListener(validator.monthTextWatcher);
-        String monthTextStr = monthText.getText().toString();
-
-        EditText dayText = findViewById(R.id.edittext_day_input);
-        dayText.addTextChangedListener(validator.dayTextWatcher);
-        String dayTextStr = dayText.getText().toString();
-
-       //initializing buttons
-        Button submitButton = findViewById(R.id.button_submittion);
-        submitButton.setOnClickListener(new OnClickListener() {
+        nameText = findViewById(R.id.edittext_name_input);
+        nameText.addTextChangedListener(new TextValidator(nameText) {
             @Override
-            public void onClick(View view) {
-                submitInfo(nameTextStr, monthTextStr, Integer.parseInt(dayTextStr));
+            public void validate(TextView textView, String text) {
+                nameValidator(text);
+                finalName = nameText.getText().toString();
             }
         });
 
-        Button resetButton = findViewById(R.id.button_resetstorage);
+        monthText = findViewById(R.id.edittext_month_input);
+        monthText.addTextChangedListener(new TextValidator(monthText) {
+            @Override
+            public void validate(TextView textView, String text) {
+                monthValidator(text);
+                finalMonth = nameText.getText().toString();
+
+            }
+        });
+
+        dayText = findViewById(R.id.edittext_day_input);
+        dayText.addTextChangedListener(new TextValidator(dayText) {
+            @Override
+            public void validate(TextView textView, String text) {
+                dayValidator(text);
+                String finalDayStr = dayText.getText().toString();
+                finalDay = Integer.parseInt(finalDayStr);
+            }
+        });
+
+        //initializing buttons
+        submitButton = findViewById(R.id.button_submittion);
+        setOnClick(submitButton, finalName, finalMonth, finalDay);
+
+        resetButton = findViewById(R.id.button_resetstorage);
         resetButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,33 +88,90 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void submitInfo(String nameTextStr, String monthTextStr, int dayTextStr) {
+    private void setOnClick(Button inButton, String inName, String inMonth, int inDay)
+    {
+        inButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dateIsValidator(inName, inDay)) {
+                    DateSet newDate = new DateSet(inMonth, inDay);
+                    PersonDatabase person = new PersonDatabase(inName, newDate);
 
-        validator.DateSetValidator(monthTextStr, dayTextStr);
-        final DateSet newDate = new DateSet(monthTextStr, dayTextStr);
-        PersonDatabase person = new PersonDatabase(nameTextStr, newDate);
+                    //adding data to persistent storage
+                    personDatabaseList.add(person);
+                    EntryCount++;
 
-        //adding data to persistent storage
-        personDatabaseList.add(person);
-        EntryCount++;
+                    //TODO: compare dateset to find similar dates then display the names
 
-        //search equal data
-        if (personDatabaseList.stream().anyMatch(o -> o.getUserBirthday().equals(newDate))) {
-            TextView outView = findViewById(R.id.textview_output_mssg);
-            outView.setText(person.getUserName());
+                    countDisplayBox = findViewById(R.id.textview_count_display);
+                    countDisplayBox.setText(personDatabaseList.size());
+                }
+            }
+        });
+    }
+
+    public void resetStorage(View view) {
+        personDatabaseList.clear();
+        EntryCount = personDatabaseList.size();
+
+        countDisplayBox.setText(EntryCount);
+        outFinalMessageBox.setText("Storage is empty.");
+    }
+
+    public boolean isAlphanumeric(String str)
+    {
+        char[] charArray = str.toCharArray();
+        for (char c : charArray) {
+            if (!Character.isLetterOrDigit(c))
+                return false;
+        }
+        return true;
+    }
+
+    public void nameValidator(String inName)
+    {
+        if(!isAlphanumeric(inName)) {
+            nameText.setError("Accept Alphanumeric only.");
+        }
+    }
+
+    public void monthValidator(String inMonth)
+    {
+        if(allMonthInNumber.contains(inMonth) == false && allMonthAbr.contains(inMonth) == false
+                && allMonthFullWord.contains(inMonth) == false){
+            monthText.setError("Input is invalid.");
+        }
+    }
+
+    public void dayValidator(String inDay)
+    {
+        if(isAlphanumeric(inDay)) {
+            dayText.setError("Accept integer only.");
+        }
+    }
+
+
+    public boolean dateIsValidator(String inMonth, int inDay)
+    {
+        boolean valid = false;
+        if(shortMonth.contains(inMonth)){
+            if(inDay < 1 && inDay > 29){
+                valid = true;
+            }
+        }
+        else if(mediumMonth.contains(inMonth)){
+            if(inDay < 1 && inDay > 30){
+                valid = true;
+            }
+        }
+        else if(longMonth.contains(inMonth)){
+            if(inDay < 1 && inDay > 31){
+                valid = true;
+            }
         }
 
-        TextView outCountView = findViewById(R.id.textview_count_display);
-        outCountView.setText(EntryCount);
+        return valid;
     }
 
-    public void resetStorage(View view){
-        personDatabaseList.clear();
-        EntryCount = 0;
 
-        TextView textView = findViewById(R.id.textview_count_display);
-        textView.setText("Empty");
-        Toast.makeText(getApplicationContext(), "Storage is cleared.",
-                Toast.LENGTH_SHORT).show();
-    }
 }
